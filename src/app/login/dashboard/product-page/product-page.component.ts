04eedfd38ba75367134c544a3c6a5db75dbbd1da
Diksha Gautam <diksha@diksha.gautam>
2020-07-22 14:48:33 +0530

    B2B varient

--- src/app/login/dashboard/product-page/product-page.component.ts
+++ src/app/login/dashboard/product-page/product-page.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import Swal from 'sweetalert2';
 import { ProductPageServiceService } from './product-page-service.service';
+import { ShopAllService } from '../shop-all/shop-all.service';
 
 
 @Component({
@@ -21,10 +22,15 @@ export class ProductPageComponent implements OnInit {
   variantTreeSampleKeys;
   variantTree;
   productIdVarient;
+  featuresModelVal = [];
+  dropDownData = ["NA"];
+
+  selectedVariantsData = [];
 
   constructor(
     readonly route: ActivatedRoute,
-    readonly service: ProductPageServiceService
+    readonly service: ProductPageServiceService,
+    readonly shopAllService: ShopAllService
   ) { }
 
   ngOnInit(): void {
@@ -54,35 +60,72 @@ export class ProductPageComponent implements OnInit {
     })
   }
 
-  getList(feature): void {
+  getList(feature, index): void {
+    let Varient;
 
     console.log("Feature -->> ", feature);
-    let Varient = this.variantTree[feature];
+    this.selectedVariantsData[index] = feature;
+    if (index === 0) {
+      Varient = this.variantTree[feature];
+    }
+    else {
+      let parentKey = this.selectedVariantsData[index - 1];
+      Varient = this.variantTree[parentKey][feature];
+    }
 
-    if(Array.isArray(Varient)){
+
+    if (Array.isArray(Varient)) {
       console.log("if statement", Varient);
       this.productIdVarient = Varient[0];
     }
-    else{
+    else {
       console.log("else statement", Varient);
+      console.log("featuresModelVal[0]", this.featuresModelVal[0]);
+      this.dropDownData.push(Varient);
     }
+    console.log("selectedVariantsData --->> ", this.selectedVariantsData)
   }
 
   addToCart(quantity): void {
 
-    if (quantity === undefined || quantity === null) {
+    if (quantity == undefined || quantity == null) {
       Swal.fire('Oops...', 'Please enter the quantity!', 'error')
     }
     else {
-      const data = {
-        'add_product_id': this.productId,
-        'quantity': quantity
-      }
 
-      this.service.addProductToCart(data).subscribe((res) => {
-        this.productData = res['data'];
-        Swal.fire("Product added successfully..!!")
-      })
+      let product;
+
+      if (this.isVirtual === 'Y') {
+        if (this.productIdVarient === undefined) {
+          Swal.fire("Oops..!!", "Please select varient", 'error');
+        }
+        else {
+          product = this.productIdVarient;
+          let data = {
+            'add_product_id': product,
+            'quantity': quantity
+          }
+
+          this.service.addProductToCart(data).subscribe((res) => {
+            this.productData = res['data'];
+            Swal.fire("Product added successfully..!!");
+            this.getCartCount();
+          })
+        }
+      }
+      else {
+        product = this.productId;
+        let data = {
+          'add_product_id': product,
+          'quantity': quantity
+        }
+
+        this.service.addProductToCart(data).subscribe((res) => {
+          this.productData = res['data'];
+          Swal.fire("Product added successfully..!!");
+          this.getCartCount();
+        })
+      }
     }
 
   }
@@ -99,6 +142,13 @@ export class ProductPageComponent implements OnInit {
     })
 
 
+  }
+
+  getCartCount(): void {
+    this.shopAllService.showCart().subscribe((res) => {
+      this.shopAllService.addValueToCart(res['data']['cart']['shoppingCartSize']);
+      this.getProductDetails();
+    })
   }
 
   scroll(id) {
