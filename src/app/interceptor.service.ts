f9e5a235f28a7f040cd8e2a0d54657e369f406d7
Diksha Gautam <diksha@diksha.gautam>
2020-07-23 17:30:59 +0530

    Interceptor

--- src/app/interceptor.service.ts
+++ src/app/interceptor.service.ts
@@ -8,17 +8,45 @@ export class APIInterceptor implements HttpInterceptor {
     url = 'https://development.skulocity.org';
 
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+
+        // if (localStorage.getItem("token")) {
+        //     req = req.clone(
+        //         { 
+        //             headers: new HttpHeaders({'Authorization': localStorage.getItem('token')}),
+        //             url: `${this.url}/${req.url}`
+        //         }
+        //     );
+        // } else {
+        //     req = req.clone({ url: `${this.url}/${req.url}`});
+        // }   
+        // return next.handle(req);
+
+
+
+        let headers = new HttpHeaders();
+        if (localStorage.getItem('token')) {
+            headers = headers.append('Authorization', localStorage.getItem('token'));
+        }
+        if (localStorage.getItem('tenantId')) {
+            headers = headers.append('tenantId', localStorage.getItem('tenantId'));
+        }
         
-        if (localStorage.getItem("token")) {
+        let request = localStorage.getItem("token");
+        if (request) {
             req = req.clone(
-                { 
-                    headers: new HttpHeaders({'Authorization': localStorage.getItem('token')}),
+                {
+                    headers: headers,
                     url: `${this.url}/${req.url}`
                 }
             );
         } else {
-            req = req.clone({ url: `${this.url}/${req.url}`});
-        }   
+            req = req.clone(
+                {
+                    headers: headers,
+                    url: `${this.url}/${req.url}`
+                }
+            );
+        }
         return next.handle(req);
     }
 }
